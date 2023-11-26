77314bdba2b5d981221796bc5cfc5ba9da7de243
Santiago Gomez Cornejo <131918914+Santos2705@users.noreply.github.com>
2023-11-26 13:04:04 -0400

    Add files via upload

---
+++ src/simulacionso/GestorUsuarios.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simulacionso;
+
+/**
+ *
+ * @author davidmizrahi
+ */
+public class GestorUsuarios {
+    
+    private Lista<Usuario> usuarios;
+    
+    
+    public GestorUsuarios(){
+        
+        this.usuarios = new Lista<Usuario>();
+        
+    }
+    
+    public void agregarUsurario(Usuario usuario){
+        
+        usuarios.agregarElemento(usuario);
+        
+    }
+
+    public Lista<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Lista<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+   
+    
+}
